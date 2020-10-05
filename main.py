872cc3c579e1b23ab003a18d2b3c5f54bf1b2636
AMG <amir.m.gholizad@gmail.com>
2020-10-05 11:39:29 +0330

    Pivot_calculator01

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+import numpy as np
+import pandas as pd
 from datetime import datetime
 
-def Pivot_Arrange(Daily)
+def Pivot_Arrange(Daily):
+    """
+    This function calculates the pivots from csv files downloaded from TSE
+    """
+    High=Daily['<HIGH>']
+    Low = Daily['<LOW>']
+    Close = Daily['<LAST>']
     x=(High[0]+Low[0])/2
     y=(High[0]+Low[0]+Close[0])/3
-    w=abs(x,y)
-    P1_high=y-w
-    P1_low=y+w
-    X=(max(High[0:N])+min(Low[0;N]))/2
-    Y=(max(High[0:N])+min(Low[0:N])+Close[0])/3
-    W=abs(X,Y)
-    P5_high=Y-W
-    P5_low=Y+W
-    retur([P1_high,P1_low,P5_high,P5_low])
+    w=abs(x-y)
+    P1_low=y-w
+    P1_high=y+w
+    X=(max(High[0:5])+min(Low[0:5]))/2
+    Y=(max(High[0:5])+min(Low[0:5])+Close[0])/3
+    W=abs(X-Y)
+    P5_low=Y-W
+    P5_high=Y+W
+    return([P1_high,P1_low,P5_high,P5_low])
+
+"""
 def Opening_Range(Instant)
     t1 = datetime.now().time()
     t2 = datetime(2020, 9, 27, 9, 15, 0).time()
@@ -21,6 +31,8 @@ def Opening_Range(Instant)
 
 def UP_DOWN(Instant)
     if Last >= OR
+"""
 
 
-
+df = pd.read_csv("S_I..N..C..Ind..csv")
+print(Pivot_Arrange(df))
