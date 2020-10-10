f48c07020e919e8a9f3a9191ef1bc43816ec5477
AMG <amir.m.gholizad@gmail.com>
2020-10-10 15:11:51 +0330

    MACRO_Calculator almost finished ;)

--- MACRO_Calculator.py
+++ MACRO_Calculator.py
@@ -23,7 +23,7 @@ def Score_Calculator(symbol,orlist,a,b,c):
 
     if (LAST < OR_high) and (LAST > OR_low):
         return 0
-    elif LAST > OR_high:
+    elif LAST >= OR_high:
         for i in range(0, len(ticks)):
             if ticks_frame["last"][i] < OR_low:
                 return 4
@@ -31,7 +31,7 @@ def Score_Calculator(symbol,orlist,a,b,c):
                 continue
         return 2
 
-    elif LAST < OR_low:
+    elif LAST <= OR_low:
         for i in range(0, len(ticks)):
             if ticks_frame["last"][i] > OR_high:
                 return -4
@@ -41,18 +41,36 @@ def Score_Calculator(symbol,orlist,a,b,c):
 
 def Macro_Calculator(a,b,c,symbol):
 
-    for j in range(0, 30):
+    Score = []
+    date = []
+    Macro_10 = []
+    adate = datetime.date(a,b,c)
+    print(adate)
+    for j in range(0, 90):
         orlist = OR_Calculator.OR_Calculator(a,b,c,symbol)
         if orlist == None:
-            c = c + 1
+            adate = adate + datetime.timedelta(days=1)
+            c = adate.day
+            b = adate.month
             continue
         #print(OR_Calculator(orlist))
-        Score = Score_Calculator(symbol,orlist,a,b,c)
-        print(Score)
-        c = c + 1
+        Score.append(Score_Calculator(symbol,orlist,a,b,c))
+        date.append(adate)
+        #print(Score , "{}-{}-{}".format(a,b,c))
+        adate = adate + datetime.timedelta(days=1)
+        c = adate.day
+        b = adate.month
+    for k in range(0,len(Score)):
+        if k<10:
+            Macro_10.append(None)
+        else:
+            Macro_10.append(sum(Score[k-9:k]))
+    MACROs_dic = { 'Date' : date ,'Score' : Score , 'Macro' : Macro_10}
+    MACROs = pd.DataFrame(MACROs_dic)
+    MACROs = MACROs.dropna()
+    print(MACROs)
 
-Macro_Calculator(2020,8,25, "آگاس")
-#date = datetime.datetime(2003,8,1,12,4,5)
+Macro_Calculator(2020,8,13, "SAFAB99")
 """
 for i in range(5):
     date = datetime.timedelta(days=1)
